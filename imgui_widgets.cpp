506f8ffddd63f0d694aee7ed2f5783e6f34f4164
ocornut <omarcornut@gmail.com>
2020-08-20 20:46:57 +0200

    RangeSelect/MultiSelect: Temporary fix/work-around for child/popup to not inherit MultiSelectEnabled flag, until we make mulit-select data stackable.

--- imgui_widgets.cpp
+++ imgui_widgets.cpp
@@ -6295,7 +6295,7 @@ bool ImGui::TreeNodeBehavior(ImGuiID id, ImGuiTreeNodeFlags flags, const char* l
     const bool was_selected = selected;
 
     // Multi-selection support (header)
-    const bool is_multi_select = g.MultiSelectEnabled;
+    const bool is_multi_select = (g.MultiSelectEnabledWindow == window);
     if (is_multi_select)
     {
         MultiSelectItemHeader(id, &selected);
@@ -6642,7 +6642,7 @@ bool ImGui::Selectable(const char* label, bool selected, ImGuiSelectableFlags fl
     if ((flags & ImGuiSelectableFlags_AllowOverlap) || (g.LastItemData.InFlags & ImGuiItemFlags_AllowOverlap)) { button_flags |= ImGuiButtonFlags_AllowOverlap; }
 
     // Multi-selection support (header)
-    const bool is_multi_select = g.MultiSelectEnabled;
+    const bool is_multi_select = (g.MultiSelectEnabledWindow == window);
     const bool was_selected = selected;
     if (is_multi_select)
     {
@@ -6952,7 +6952,7 @@ ImGuiMultiSelectData* ImGui::BeginMultiSelect(ImGuiMultiSelectFlags flags, void*
     ImGuiContext& g = *GImGui;
     ImGuiWindow* window = g.CurrentWindow;
 
-    IM_ASSERT(g.MultiSelectEnabled == false);   // No recursion allowed yet (we could allow it if we deem it useful)
+    IM_ASSERT(g.MultiSelectEnabledWindow == NULL);   // No recursion allowed yet (we could allow it if we deem it useful)
     IM_ASSERT(g.MultiSelectFlags == 0);
     IM_ASSERT(g.MultiSelectState.FocusScopeId == 0);
 
@@ -6961,7 +6961,7 @@ ImGuiMultiSelectData* ImGui::BeginMultiSelect(ImGuiMultiSelectFlags flags, void*
     ms->Clear();
     ms->FocusScopeId = window->IDStack.back();
     PushFocusScope(ms->FocusScopeId);
-    g.MultiSelectEnabled = true;
+    g.MultiSelectEnabledWindow = window;
     g.MultiSelectFlags = flags;
 
     // Use copy of keyboard mods at the time of the request, otherwise we would requires mods to be held for an extra frame.
@@ -7008,7 +7008,7 @@ ImGuiMultiSelectData* ImGui::EndMultiSelect()
         ms->Out.RangeValue = true;
     g.MultiSelectState.FocusScopeId = 0;
     PopFocusScope();
-    g.MultiSelectEnabled = false;
+    g.MultiSelectEnabledWindow = NULL;
     g.MultiSelectFlags = ImGuiMultiSelectFlags_None;
 
 #ifdef IMGUI_DEBUG_MULTISELECT
