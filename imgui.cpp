6ecb2c5d4fb0ea2a7a2200a92db21df241e327e9
ocornut <omarcornut@gmail.com>
2023-10-26 17:20:33 +0200

    RangeSelect/MultiSelect: support for nested/stacked BeginMultiSelect().
    Mimicking table logic, reusing buffers.

--- imgui.cpp
+++ imgui.cpp
@@ -3692,6 +3692,7 @@ void ImGui::Shutdown()
     g.DrawChannelsTempMergeBuffer.clear();
 
     g.MultiSelectStorage.Clear();
+    g.MultiSelectTempData.clear_destruct();
 
     g.ClipboardHandlerData.clear();
     g.MenusIdSubmittedThisFrame.clear();
@@ -3801,6 +3802,8 @@ void ImGui::GcCompactTransientMiscBuffers()
     ImGuiContext& g = *GImGui;
     g.ItemFlagsStack.clear();
     g.GroupStack.clear();
+    g.MultiSelectTempDataStacked = 0;
+    g.MultiSelectTempData.clear_destruct();
     TableGcCompactSettings();
 }
 
